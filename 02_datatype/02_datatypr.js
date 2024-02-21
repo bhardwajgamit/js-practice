dc4ad2f834fe483a1ae224171ed7e975b1b9fbe7
gamit bhardwaj <125485498+bhardwajgamit@users.noreply.github.com>
2024-02-21 06:37:44 +0000

    datatype

---
+++ 02_datatype/02_datatypr.js
@@ -0,0 +1,12 @@
+"use strict"//treat alll code as new version
+
+alert(4+4)// we are use node js not browser that why this gave error in  in node js
+
+console.log("bhardwaj")
+
+//nuuber
+//string
+//bolean
+//undefine
+
+//null
