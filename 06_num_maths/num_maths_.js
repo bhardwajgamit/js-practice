d7c08ad4c82149b41b125bc5df6529cf90821398
gamit bhardwaj <125485498+bhardwajgamit@users.noreply.github.com>
2024-02-28 06:00:02 +0000

    num and math

---
+++ 06_num_maths/num_maths_.js
@@ -0,0 +1,27 @@
+// const balance=400;
+
+
+
+// const balance1=new Number(800)
+// console.log(balance1)
+
+// console.log(balance1.toString().length)
+
+
+// console.log(balance.toFixed(2))
+
+
+// const big_=100000
+// console.log(big_.toLocaleString("en-IN"))
+
+
+
+//===========================MATHS============================
+
+
+console.log((Math.random()*10)+1)
+
+
+const min= 10
+const max=40
+Math.random()*(max-min+1)
