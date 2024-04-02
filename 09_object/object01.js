2399f7bde5b2686f2f40c4ea398f85d58eb3e489
gamit bhardwaj <125485498+bhardwajgamit@users.noreply.github.com>
2024-04-02 05:54:32 +0000

    obj1

---
+++ 09_object/object01.js
@@ -0,0 +1,9 @@
+const jsuser={
+    name:"bhardwaj",
+    age:27,
+    location:"surat",
+    email:"bhardwaj1@gmail.com",
+    isloggedin:false,
+    lastloginday:["monday","saturday"]
+}
+console.log(jsuser.name);
